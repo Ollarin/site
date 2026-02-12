0443a213c7a89d015b99249e391dae467314f75e
Raveen Rajadorai <admiralra@gmail.com>
2026-02-12 14:53:46 +0000

    Quartz sync: Feb 12, 2026, 2:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Tech Art - Ollarin",
+    pageTitle: "Ollarin's tech art blog",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
