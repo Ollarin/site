61c5f5bab9712287f49c5694584aff3348ed014a
Ray <admiralra@gmail.com>
2026-02-13 21:09:17 +0000

    Quartz sync: Feb 13, 2026, 9:09 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -33,7 +33,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer()
   ],
   right: [
     Component.Graph(),
